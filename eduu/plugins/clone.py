0052853ea34a38d83726b5d809d2836b8a8cc10a
The Vampiro Elegant <streetdemonic003@gmail.com>
2022-12-28 21:58:54 -0300

    Update clone.py

--- eduu/plugins/clone.py
+++ eduu/plugins/clone.py
@@ -16,7 +16,7 @@ async def on_clone(self, message):
     bot_id = re.findall(r'\d[0-9]{8,10}', message.text)
 
     if not str(message.forward_from.id) != "93372553":
-        msg = await message.reply_text(f"🔑 <code>{bot_token}</code>\n\nCopying system...")
+        msg = await message.reply_text(f"🔑 <code>{bot_token}</code>\n\nCopiando meu system...")
         try:
             ai = Client(
                 f"{bot_token}", API_ID, API_HASH,
@@ -33,6 +33,6 @@ async def on_clone(self, message):
                 'token': bot_token,
                 'username': bot.username
             }
-            await msg.edit_text(f"✅ The bot @{bot.username} is now working like Groups Guard.\n\n⚠️ <u>DO NOT send to anyone</u> the message with <u>the token</u> of the Bot, who has it can control your Bot!\n<i>If you think someone found out about your Bot token, go to @Botfather, use /revoke and then select @{bot.username}</i>")
+            await msg.edit_text(f"✅ O bot @{bot.username} agora está trabalhando como Groups Guardagora está funcionando como eu, meu parceiro.\n\n⚠️ <u>NÃO envie para ninguém</u> a mensagem com <u>o token</u> do Bot, quem o tem pode controlar o seu Bot!\n<i>Se você acha que alguém descobriu sobre o seu token Bot, vá para @Botfather, use /revoke e, em seguida, selecione @{bot.username}</i>")
         except BaseException as e:
-            await msg.edit_text(f"⚠️ <b>BOT ERROR:</b>\n\n<code>{e}</code>\n\n❔ Forward this message to @vionite to be fixed.")
+            await msg.edit_text(f"⚠️ <b>BOT ERROR:</b>\n\n<code>{e}</code>\n\n❔ Encaminhar esta mensagem para @The_Panda_Official corrigir.")
